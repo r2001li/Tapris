35a364f43373ad0bff4f4a524586318459735f17
Nero <ruiang2001@gmail.com>
2022-12-01 19:51:35 -0500

    Setup complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { Client, Events, GateWayIntentBits } = require("discord.js");
-const { token } = require("config.json");
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
 
-const client = new Client({ intents: [GateWayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, (c) => {
   console.log(`${c.user.tag} online.`);
